83b0e3398a4c638b01d07e94d451b7b95785a0d1
vaibhavig <vaibhavigovankop@gmail.com>
2023-07-15 15:51:19 +0530

    First commit

---
+++ newslettter.php
@@ -0,0 +1,39 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								
+								<li>
+									<a href="https://twitter.com/VGovankop?t=3fkcl15cdj2n2MTR--MkEg&s=09"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://instagram.com/vaibhavi_gvp?igshid=NGExMmI2YTkyZg=="><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/vaibhavigvp"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
